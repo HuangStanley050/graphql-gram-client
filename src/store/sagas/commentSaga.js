1ff89578dd1150b09edde3686f5fe5c6eb25708c
Stanley Huang <huang.stanley050@gmail.com>
2019-07-11 09:55:22 +1000

    might have solve it, have sagaworker check postChanged state before fetching

--- src/store/sagas/commentSaga.js
+++ src/store/sagas/commentSaga.js
@@ -1,11 +1,8 @@
-import { takeEvery, put, select } from "redux-saga/effects";
+import {takeEvery, put, select} from "redux-saga/effects";
 import * as actionType from "../actions/actionTypes";
-import {
-  get_comments_okay,
-  get_comments_fail
-} from "../actions/commentActions";
+import {get_comments_okay, get_comments_fail} from "../actions/commentActions";
 import API from "../../constants/API";
-import { getCurrentPost } from "./getState";
+import {getCurrentPost, getPostStatus} from "./getState";
 import axios from "axios";
 const api_path = API.api_path;
 
@@ -17,14 +14,16 @@ function* fetchCommentsSagaWorker(action) {
   //yield console.log(action);
   const token = localStorage.getItem("graphgram-token");
   let postId = yield select(getCurrentPost);
-
-  try {
-    let result = yield axios({
-      headers: { Authorization: "bearer " + token },
-      method: "post",
-      url: api_path,
-      data: {
-        query: `
+  let postStatus = yield select(getPostStatus); //let saga knows if the postChanged status is true or false
+  //console.log(postStatus);
+  if (postStatus) {
+    try {
+      let result = yield axios({
+        headers: {Authorization: "bearer " + token},
+        method: "post",
+        url: api_path,
+        data: {
+          query: `
             query {
               comments(postId:"${postId}") {
                 id
@@ -34,13 +33,15 @@ function* fetchCommentsSagaWorker(action) {
               }
             }
         `
-      }
-    });
-    //console.log(result.data.data.comments);
-    yield put(get_comments_okay(result.data.data.comments));
-  } catch (e) {
-    console.log(e);
-  }
+        }
+      });
+      //console.log(result.data.data.comments);
+      yield put(get_comments_okay(result.data.data.comments));
+      yield put({type: "POST_NO_CHANGE"});
+    } catch (e) {
+      console.log(e);
+    }
+  } else return;
 }
 
 export default commentSagaWatcher;
--- src/store/sagas/getState.js
+++ src/store/sagas/getState.js
@@ -1,2 +1,3 @@
 export const getCurrentPost = state => state.post.currentPost;
 export const getUserId = state => state.auth.userData.userId;
+export const getPostStatus = state => state.post.postChanged;
