d0135b553eec72a2541b5a149f0715ff46971e39
Stanley Huang <huang.stanley050@gmail.com>
2019-07-23 22:43:14 +1000

    bug is the image in carousel doesn't have w-100 class for some reason

--- src/components/slideShow.js
+++ src/components/slideShow.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, {Component} from "react";
 import {connect} from "react-redux";
 import {get_ownFiles_start} from "../store/actions/ownFilesActions";
 import {
@@ -27,47 +27,85 @@ const items = [
     src:
       "data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa21%20text%20%7B%20fill%3A%23333%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa21%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23555%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22277%22%20y%3D%22218.3%22%3EThird%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E",
     altText: "Slide 3",
-    caption: "Slide3"
+    caption: "Slide 3"
   }
 ];
 
-const SlideShow = props => {
-  //props.getFiles(props.userId);
-  const [activeIndex, setActiveIndex] = useState(0);
-
-  const onExiting = () => (props.animating = true);
-  const onExited = () => (props.animating = false);
-  const next = () => {
-    if (props.animating) return;
-    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
-    setActiveIndex(nextIndex);
-    //this.setState({ activeIndex: nextIndex });
-  };
-  const previous = () => {
-    if (props.animating) return;
-    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
-    setActiveIndex(nextIndex);
-  };
-  const goToIndex = newIndex => {
-    if (props.animating) return;
-    setActiveIndex(newIndex);
-    //this.setState({ activeIndex: newIndex });
+class SlideShow extends Component {
+  state = {
+    activeIndex: 0
   };
 
-  const slides = items.map(item => {
-    return (
-      <CarouselItem onExiting={onExiting} onExited={onExited} key={item.src}>
-        <img src={item.src} alt={item.altText} />
-        <CarouselCaption
-          captionText={item.caption}
-          captionHeader={item.caption}
+  onExiting = () => (this.animating = true);
+  onExited = () => (this.animating = false);
+  next = () => {
+    if (this.animating) return;
+    const nextIndex =
+      this.state.activeIndex === items.length - 1
+        ? 0
+        : this.state.activeIndex + 1;
+
+    this.setState({activeIndex: nextIndex});
+  };
+  previous = () => {
+    if (this.animating) return;
+    const nextIndex =
+      this.state.activeIndex === 0
+        ? items.length - 1
+        : this.state.activeIndex - 1;
+    this.setState({activeIndex: nextIndex});
+  };
+  goToIndex = newIndex => {
+    if (this.animating) return;
+
+    this.setState({activeIndex: newIndex});
+  };
+
+  render() {
+    const {activeIndex} = this.state;
+    const slides = items.map(item => {
+      return (
+        <CarouselItem
+          onExiting={this.onExiting}
+          onExited={this.onExited}
+          key={item.src}
+        >
+          <img className="w-100" src={item.src} alt={item.altText} />
+          <CarouselCaption
+            captionText={item.caption}
+            captionHeader={item.caption}
+          />
+        </CarouselItem>
+      );
+    });
+    const controlledCaro = (
+      <Carousel
+        activeIndex={activeIndex}
+        next={this.next}
+        previous={this.previous}
+      >
+        <CarouselIndicators
+          items={items}
+          activeIndex={activeIndex}
+          onClickHandler={this.goToIndex}
         />
-      </CarouselItem>
+        {slides}
+        <CarouselControl
+          direction="prev"
+          directionText="Previous"
+          onClickHandler={this.previous}
+        />
+        <CarouselControl
+          direction="next"
+          directionText="Next"
+          onClickHandler={this.next}
+        />
+      </Carousel>
     );
-  });
-
-  return <UncontrolledCarousel items={items} />;
-};
+    const uncontrolledCaro = <UncontrolledCarousel items={items} />;
+    return controlledCaro;
+  }
+}
 
 const mapDispatchToProps = dispatch => {
   return {
